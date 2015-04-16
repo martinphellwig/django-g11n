513c9f31ffd481f46404bee73e960887d03d25b8
hellwig <hellwig@localhost>
2015-04-16 14:19:28 +0100

    Make modules more PyLint compliant.

--- ipinfo/_database.py
+++ ipinfo/_database.py
@@ -1,4 +1,6 @@
-import os
+"""
+Using a sqlite database for fast lookup.
+"""
 import csv
 import sqlite3
 import ipaddress
@@ -10,6 +12,7 @@ from _ip2country import write_csv
 DBN = 'ipdb.sq3'
 
 class Database(object):
+    "Database abstraction."
     def __init__(self, filename=None):
         if filename == None:
             filename = write_csv()
@@ -18,25 +21,25 @@ class Database(object):
         self.cnx = sqlite3.connect(DBN)
         self.cursor = self.cnx.cursor()
         self._create_table(table)
-    
+
         with open(filename) as file_open:
             reader = csv.reader(file_open)
-            for i in range(40):
-                next(reader)
+            next(reader) # skip header
             data = list(reader)
             self._insert_data(table, data)
-                        
+
     def _insert_data(self, table, data):
+        "Insert data where appropiate"
         rids_data = set()
         for row in data:
             rids_data.add(row[0])
-            
+
         rids_dbir = set()
-        
+
         sql = "SELECT rid FROM %s" % table
         for row in self.cursor.execute(sql):
             rids_dbir.add(row[0])
-            
+
         remove_from_db = rids_dbir.difference(rids_data)
         if len(remove_from_db) > 0:
             print('# Removing old records from database.')
@@ -45,30 +48,32 @@ class Database(object):
             self.cursor.execute(sql, list(remove_from_db))
         else:
             print('# No stale records found in database.')
-        
+
         insert_into_db = rids_data.difference(rids_dbir)
         if len(insert_into_db) > 0:
             print('# Inserting new records in database')
             inserts = list()
             for row in data:
                 if row[0] in insert_into_db:
+                    row[-1] = str(row[-1])
+                    row[-2] = str(row[-2])
                     inserts.append(row)
-                    
-            sql = """INSERT INTO ipranges(rid, nic, tldcc, ip_version, 
+
+            sql = """INSERT INTO ipranges(rid, nic, tldcc, ip_version,
                                           network, broadcast) 
                     VALUES (?, ?, ?, ?, ?, ?)"""
             self.cursor.executemany(sql, inserts)
         else:
             print('# No new records found.')
         self.cnx.commit()
-        
-        
-            
+
     def _has_table(self, name):
+        "Do we have the table in the database"
         meta = self.cursor.execute("PRAGMA table_info('%s')" % name)
-        return len(meta.fetchall()) > 0    
-                
+        return len(meta.fetchall()) > 0
+
     def _create_table(self, name):
+        "Create table"
         if self._has_table(name):
             return
         sql = """
@@ -77,38 +82,45 @@ class Database(object):
              nic         TEXT,
              tldcc       TEXT,
              ip_version  INTEGER,
-             network     INTEGER,
-             broadcast   INTEGER)
+             network     BLOB,
+             broadcast   BLOB)
         """ % name
-        
+
         self.cursor.execute(sql)
-    
+
     def get_record(self, address):
-        self.cursor.execute("SELECT * FROM ipranges")
-        rows = self.cursor.fetchall()
-        
+        "Get the record from the database"
         address = ipaddress.ip_address(address)
 
         if address.version == 4:
-            af = socket.AF_INET
+            _af = socket.AF_INET
         else:
-            af = socket.AF_INET6
+            _af = socket.AF_INET6
 
-        packed_string = socket.inet_pton(af, address.exploded)
+        packed_string = socket.inet_pton(_af, address.exploded)
         integer = int(binascii.hexlify(packed_string), 16)
-        
-        sql = "SELECT * FROM ipranges WHERE network<? AND broadcast>?"
-        self.cursor.execute(sql, [integer, integer])
+
+        sql = """
+        SELECT * FROM ipranges WHERE
+                network < ? 
+            AND broadcast > ?
+            AND LENGTH(network) <= ?
+            AND LENGTH(broadcast) >= ?"""
+        item = str(integer)
+        length = len(item)
+
+        self.cursor.execute(sql, [item, item, length, length])
         return self.cursor.fetchall()
-    
+
     def return_tldcc(self, address):
+        "Just return the country code"
+        # it is actually TLDcc which can also include regions like EU
         return self.get_record(address)[0][2]
-    
-    
-    
+
 if __name__ == '__main__':
-    database = Database()
-    print(database.return_tldcc('213.158.89.3'))
+    database = Database('/tmp/ip_country_range_2015_04_16T11_42_35_001479.csv')
+    print(database.return_tldcc('221.192.143.171'))
+    
 
         
         
--- ipinfo/_ip2country.py
+++ ipinfo/_ip2country.py
@@ -1,3 +1,6 @@
+"""
+Module to fetch and parse regional NIC delegation data
+"""
 import urllib.parse
 import ftplib
 import os
@@ -46,7 +49,7 @@ def _file_details(ftp, file_name):
                 details['name_local'] = name
                 details['name_remote'] = name
                 break
-                
+
     except ftplib.error_perm:
         print('# Server does not support mlsd, falling back.')
         tmp = list()
@@ -56,7 +59,7 @@ def _file_details(ftp, file_name):
             link2name = tmp[0].split('->')[1].strip()
             tmp = list()
             ftp.retrlines('LIST %s' % link2name, callback=tmp.append)
-        
+
         details = dict()
         tmp = tmp[0]
         tmp = tmp.rsplit(' ', 1)[0]
@@ -64,63 +67,63 @@ def _file_details(ftp, file_name):
         details['name_remote'] = link2name
         tmp, details['size'], month, day, time = tmp.rsplit(' ', 4)
         details['modify'] = '_'.join([month, day, time.replace(':', '')])
-        
-    return details        
+
+    return details
 
 def download(url):
     "Download the url."
     host, file_path, file_name = _split_url(url)
-    
+
     print('# Connecting to: %s' % host)
     ftp = ftplib.FTP(host)
-    
+
     print('# Logging in ...')
     ftp.login()
-    
+
     print('# Changing cwd to: %s' % file_path)
     ftp.cwd(file_path)
-    
+
     details = _file_details(ftp, file_name)
     if details == None:
         print('# No updated files found !')
         return
-            
-    file_cache = '_'.join([details['name_local'], 
-                           details['size'], 
+
+    file_cache = '_'.join([details['name_local'],
+                           details['size'],
                            details['modify']])
     file_cache += '.csv'
-    
+
     if file_cache in os.listdir(TWD):
         print('# File is already downloaded !')
         return
-    
+
     print('# Downloading ...')
-    
+
     retr = 'RETR %s' % details['name_remote']
     local_file = os.path.join(TWD, file_cache)
     ftp.retrbinary(retr, open(local_file, 'wb').write)
-    
-    print('# Downloaded!') 
+
+    print('# Downloaded!')
 
 
-# The parsing part of the program   
+# The parsing part of the program
 def _address_range_ipv4(address, width):
     "Convert IPv4 address and amount to integer range."
     # The width of ipv4 addresses is given in number of addresses which
     # are not bounded by exact netmasks for example a width of 640 addresses.
     blocks = address.split('.')
     for index, block in enumerate(blocks):
-        blocks[index] = bin(int(block, 10))[2::].zfill(8) 
+        blocks[index] = bin(int(block, 10))[2::].zfill(8)
     blocks = ''.join(blocks)
     network = int(blocks, 2)
     broadcast = network + int(width) - 1
     return(network, broadcast)
-        
+
 def _ipv6_to_int(ipv6_address):
     "Convert an IPv6 address to an integer"
     packed_string = socket.inet_pton(socket.AF_INET6, ipv6_address.exploded)
     return int(hexlify(packed_string), 16)
-    
+
 def _address_range_ipv6(address, width):
     "Convert IPv6 address and broadcast to integer range."
     network = ipaddress.ip_network(address+'/'+width)
@@ -137,7 +140,7 @@ def _address_range(ipv, address, width):
     else:
         # IPv6, width is given by a netmask.
         network, broadcast = _address_range_ipv6(address, width)
-        
+
     return (network, broadcast)
 
 def _parse_row(row):
@@ -151,61 +154,67 @@ def _parse_row(row):
             # This is the country it is assigned to, if there is no country
             # I am not interested in it.
             return None
-        
+
         if tmp[2].strip().lower() not in ['ipv4', 'ipv6']:
             # If the protocol is not an IP protocol (such as asn), I am not
             # interested.
             return None
-        
+
         if '6' in tmp[2]:
             tmp[2] = 6
         else:
             tmp[2] = 4
-        
+
         # Convert the IP address and netmask/number of IP's to an IP range where
         # the IPs are converted to a numerical value.
         tmp[3], tmp[4] = _address_range(tmp[2], tmp[3], tmp[4])
         return tmp
 
-    
+
 class CompactRanges(object):
     "Try to compact the ranges."
     def __init__(self):
         self.ranges = list()
-            
-    def add(self, nic, tld, ipv, network, broadcast):
+
+    def add(self, *newer):
         "Add a line to the ranges, compacting where possible."
-        newer = [nic, tld, ipv, network, broadcast]
+        # nic, tld, ipv, network, broadcast = *newer
+
         if len(self.ranges) == 0:
             self.ranges.append(newer)
             return
-        
+
         # Testing if current range is a continuation of the previous one
         older = self.ranges[-1]
         if older[0] == newer[0] and \
            older[1] == newer[1] and \
            older[2] == newer[2] and \
-           older[4] == network - 1: 
+           older[4] == newer[3] - 1:
             # The older broadcast is the same as newer network - 1, thus is is a
             # continuation, so extending the range of the older one.
-            self.ranges[-1][4] = broadcast
+            self.ranges[-1][4] = newer[4]
         else:
-            self.ranges.append(newer)                
+            self.ranges.append(newer)
+
+    def length(self):
+        "return length of ranges"
+        return len(self.ranges)
 
 
 def _local_file_from_url(url):
     "Open the file, if available from the url"
-    host, file_path, file_name = _split_url(url)
+    file_name = _split_url(url)[2]
     candidates = list()
+
     for candidate in os.listdir(TWD):
         if file_name.lower() in candidate.lower():
             candidates.append(candidate)
-            
+
     candidates.sort(reverse=True)
     if len(candidates) == 0:
         print('# No files to parse')
         return None
-        
+
     file_full = os.path.join(TWD, candidates[0])
     return file_full
 
@@ -217,7 +226,7 @@ def parse_latest(url):
         return
 
     print('# Opening file: %s' % file_name)
-     
+
     compacted = CompactRanges()
     count_linesall = 0
     count_relevant = 0
@@ -227,34 +236,15 @@ def parse_latest(url):
             parsed = _parse_row(row)
             if parsed == None:
                 continue
-            
+
             count_relevant += 1
+            # pylint: disable=star-args
             compacted.add(*parsed)
-    
+
     print('# Parsed %s lines' % count_linesall)
     print('#  - of which relevant: %s' % count_relevant)
-    print('#  - reduced to ranges: %s' % len(compacted.ranges))
+    print('#  - reduced to ranges: %s' % compacted.length())
     return compacted.ranges
-
-def get_data():
-    print('#'*79)
-    print('# Fetching data from regional NICs.')
-    print('#'*79)
-    tmp = list()
-    for delegate in DELEGATES:
-        print('# Using: %s' % delegate)
-        download(delegate)
-        tmp += parse_latest(delegate)
-        print('#' * 79)
-    print('# A total of %s IP ranges have been defined.' % len(tmp))
-    
-    for row in tmp:
-        nic, country, ipv, network, broadcast = row
-        rid = nic[:2]+country+str(ipv)+hex(broadcast)[2::]+hex(network)[2::]
-        rid = rid.lower()
-        rid = _compact_string(rid)
-        yield rid, nic, country, ipv, network, broadcast
-        
 
 def _compact_string(text):
     "try making text compacter"
@@ -266,7 +256,7 @@ def _compact_string(text):
     tmp = list()
     last = ''
     count = 0
-    for character in text+'_': 
+    for character in text+'_':
         # Add the underscore so we make sure not to miss the last bit of the
         # string if it happens to end on more then 4 identical characters.
         count += 1
@@ -274,15 +264,34 @@ def _compact_string(text):
             if count > 4:
                 tmp = tmp[:len(tmp)-count]
                 tmp.append('_%s_%s_' % (last, count))
-                
+
             count = 0
-        
+
         last = character
         tmp.append(character)
-        
+
     # Remove the appended underscore before returning.
-    return ''.join(tmp)[:-1]           
-    
+    return ''.join(tmp)[:-1]
+
+def get_data():
+    "Fetch and parse data"
+    print('#'*79)
+    print('# Fetching data from regional NICs.')
+    print('#'*79)
+    tmp = list()
+    for delegate in DELEGATES:
+        print('# Using: %s' % delegate)
+        download(delegate)
+        tmp += parse_latest(delegate)
+        print('#' * 79)
+    print('# A total of %s IP ranges have been defined.' % len(tmp))
+
+    for row in tmp:
+        nic, country, ipv, network, broadcast = row
+        rid = nic[:2]+country+str(ipv)+hex(broadcast)[2::]+hex(network)[2::]
+        rid = rid.lower()
+        rid = _compact_string(rid)
+        yield rid, nic, country, ipv, network, broadcast
 
 def write_csv(file_name = None):
     "Create a CSV from the data"
@@ -293,16 +302,13 @@ def write_csv(file_name = None):
     if file_name == None:
         name = "ip_country_range_" + stamp + '.csv'
         file_name = os.path.join(TWD, name)
-    
+
     header = ['rid', 'nic', 'tldcc', 'ip_version', 'network', 'broadcast']
     data = get_data()
     with open(file_name, 'w') as file_open:
         writer = csv.writer(file_open)
         writer.writerow(header)
         writer.writerows(data)
-    
+
     print('# Written a csv to: %s' % file_name)
     return file_name
-    
-if __name__ == '__main__':
-    write_csv()
