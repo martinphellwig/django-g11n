2d4660a2fda6f62f35bed50d8ba485562f1be35a
hellwig <hellwig@localhost>
2016-06-27 11:21:32 +0100

    Improved coverage , some refactoring and code deletion.

--- django_g11n/tests/test_007_request.py
+++ django_g11n/tests/test_007_request.py
@@ -9,6 +9,19 @@ class VariousTestCase(TestCase):
         client = Client()
         client.get('', REMOTE_ADDR='109.74.193.121')
 
+    def test_002_by_request_ipaddress(self):
+        from ..tools import by_request
+        import os
+        os.environ['G11N_IP_OVERRIDE'] = ''
+        self.assertRaises(ValueError, by_request.ipaddress, None)
+
+    def test_003_by_request_ipaddress(self):
+        from ..tools import by_request
+        import os
+        address = '109.74.193.121'
+        os.environ['G11N_IP_OVERRIDE'] = address
+        expected = {'ip':address}
+        self.assertEqual(expected, by_request.ipaddress(None))
 
 if __name__ == '__main__': # pragma: no cover
     # pylint: disable=wrong-import-position
